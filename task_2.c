c2b730490137773d707ae502f15e3f84c7dd23f4
Erik Andrén <90768963+egobling@users.noreply.github.com>
2023-12-12 11:17:12 +0100

    Add files via upload

---
+++ task_2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+	int r = 1;
+	float pi = 3.1415;
+	
+	float area = pi * r*r;
+	float Omkrets = 2*pi*r;
+	
+	printf("%f\n",area);
+	printf("%f\n",Omkrets);
+	return 0;
+	}
